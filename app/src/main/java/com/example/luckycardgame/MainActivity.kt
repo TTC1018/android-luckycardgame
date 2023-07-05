f9e19bab6cb5c28388fc8b51fee2a0bb3147bc8a
DongHyeon Park <paramore@kakao.com>
2023-07-05 11:53:15 +0900

    issue #26 feat: 인원 변경 시 icon 및 카드 영역 보임/숨김처리

--- app/src/main/java/com/example/luckycardgame/MainActivity.kt
+++ app/src/main/java/com/example/luckycardgame/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.luckycardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
 import com.example.luckycardgame.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -18,5 +20,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        addToggleChangedListener()
+    }
+
+    private fun addToggleChangedListener() {
+        binding.tgButtons.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            val toggleBtn = group.findViewById<MaterialButton>(checkedId)
+            val userNum = toggleBtn.text.first().digitToInt()
+            when (isChecked) {
+                true -> {
+                    toggleBtn.icon = AppCompatResources.getDrawable(this, R.drawable.ic_check)
+                    viewModel.setUserNum(userNum)
+                }
+                false -> toggleBtn.icon = null
+            }
+        }
     }
 }
--- app/src/main/java/com/example/luckycardgame/MainActivityViewModel.kt
+++ app/src/main/java/com/example/luckycardgame/MainActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.luckycardgame
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,4 +9,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(): ViewModel() {
+    private val TAG = this.javaClass.simpleName
+
+    private val _userNum = MutableLiveData<Int>()
+    val userNum: LiveData<Int> get() = _userNum
+
+    fun setUserNum(num: Int) {
+        _userNum.value = num
+    }
 }
