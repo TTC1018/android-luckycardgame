6d4bec4ec24c328aac7ecc4e00b51a1007afe76c
DongHyeon Park <paramore@kakao.com>
2023-07-05 11:18:11 +0900

    issue #26 feat: Hilt 적용

--- app/src/main/java/com/example/luckycardgame/MainActivity.kt
+++ app/src/main/java/com/example/luckycardgame/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.luckycardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.luckycardgame.data.Card
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.luckycardgame.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val TAG = this.javaClass.simpleName
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        Log.d(TAG, Array(10) { Card() }.joinToString(", "))
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
