85ef1d38fdbf9cf72a168777a7807dd183fbd261
DongHyeon Park <paramore@kakao.com>
2023-07-07 11:23:22 +0900

    issue #19 refactor: 일부 코드 개행 추가

--- app/src/main/java/com/example/luckycardgame/MainActivity.kt
+++ app/src/main/java/com/example/luckycardgame/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.luckycardgame.data.Card
 class MainActivity : AppCompatActivity() {
 
     private val TAG = this.javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
